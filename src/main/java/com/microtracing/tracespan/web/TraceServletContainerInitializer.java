2a55d8ea89a6cc5fb36b7a0e47b483ed3547d526
zhaowh <zhaowh@gmail.com>
2017-10-13 23:34:39 +0800

    add TraceServletContainerInitializer

---
+++ src/main/java/com/microtracing/tracespan/web/TraceServletContainerInitializer.java
@@ -0,0 +1,36 @@
+package com.microtracing.tracespan.web;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HandlesTypes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@HandlesTypes({ TraceFilter.class }) 
+public class TraceServletContainerInitializer implements ServletContainerInitializer {
+	private static final Logger logger =  LoggerFactory.getLogger(TraceFilter.class);
+
+	private static final String FILTER_MAPPING = "/*"; 
+	
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+	    logger.info("add filter : " + TraceFilter.class.getName()); 
+	    FilterRegistration.Dynamic filter = ctx.addFilter( 
+	    		TraceFilter.class.getSimpleName(), TraceFilter.class); 
+
+	    EnumSet<DispatcherType> dispatcherTypes = EnumSet 
+	        .allOf(DispatcherType.class); 
+	    dispatcherTypes.add(DispatcherType.REQUEST); 
+	    dispatcherTypes.add(DispatcherType.FORWARD); 
+
+	    filter.addMappingForUrlPatterns(dispatcherTypes, true, FILTER_MAPPING); 
+	}
+
+}
